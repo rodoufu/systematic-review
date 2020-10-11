b70649f5932cbef63006a69e656cca1c04e23c77
Rodolfo Araujo <rodoufu@gmail.com>
2020-10-11 12:11:11 -0300

    Cache ignore case.

--- systematic-review/cache.py
+++ systematic-review/cache.py
@@ -9,7 +9,8 @@ from typing import Dict, NoReturn, Set, Union, Iterable
 
 
 class SearchCache(object):
-	def __init__(self):
+	def __init__(self, ignore_case: bool = True):
+		self.ignore_case = ignore_case
 		self.data: Dict[Source, Dict[SearchRequest, Set[SearchResponse]]] = {}
 
 	def __contains__(self, item: SearchRequestSource) -> bool:
@@ -25,19 +26,27 @@ class SearchCache(object):
 			raise KeyError(f"Request {item.request} not found for source: {item.source}")
 
 	def __getitem__(self, item: SearchRequestSource) -> Set[SearchResponse]:
+		if self.ignore_case:
+			item.request.value = item.request.value.lower()
 		self.__check(item)
 		return self.data[item.source][item.request]
 
 	def __setitem__(self, item: SearchRequestSource, value: SearchResponse) -> NoReturn:
+		if self.ignore_case:
+			item.request.value = item.request.value.lower()
 		self.data[item.source] = self.data.get(item.source, {})
 		self.data[item.source][item.request] = self.data[item.source].get(item.request, set())
 		self.data[item.source][item.request].add(value)
 
 	def __delitem__(self, key: Union[SearchRequestSource, SearchResponse]) -> NoReturn:
 		if type(key) is SearchRequestSource:
+			if self.ignore_case:
+				key.request.value = key.request.value.lower()
 			self.__check(key)
 			del self.data[key.source][key.request]
 		elif type(key) is SearchResponse:
+			if self.ignore_case:
+				key.request_source.request.value = key.request_source.request.value.lower()
 			self.__check(key.request_source)
 			obj = self.data[key.request_source.source][key.request_source.request]
 			if key not in obj:
