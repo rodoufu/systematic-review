a7317c52d62d10782df5fedc9b5e342f26cc54a6
Rodolfo Araujo <rodoufu@gmail.com>
2020-10-11 12:10:36 -0300

    Using event loop.

--- systematic-review/__main__.py
+++ systematic-review/__main__.py
@@ -107,8 +107,7 @@ async def main():
 
 
 if __name__ == '__main__':
-	# loop = asyncio.get_event_loop()
-	# asyncio.set_event_loop(loop)
+	loop = asyncio.get_event_loop()
+	asyncio.set_event_loop(loop)
 
-	# loop.run_until_complete(main())
-	asyncio.run(main())
+	loop.run_until_complete(main())
